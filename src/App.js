a5ebd9ed91925984abe6506bfd05a4acb3a893bc
Ben Daniel-Greep <bendanielgreep@gmail.com>
2021-09-23 12:05:05 +0100

    basic styling done on cards

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import Container from 'react-bootstrap/Container';
+import Card from 'react-bootstrap/Card';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image';
@@ -24,20 +25,22 @@ const App = () => {
   return (
     <Container className='app' >
           <Row>
-            <Col  xs={6} md={4}>
+
       {
         images && images.map(img => (
-          // <div key={img.id} >
-          //   <img src={`${img.url}.jpg`} alt=''/>
-          //   <img src={`${img.user.profile_image}.webp`} alt=''/>
-          // </div>
-              <div key={img.id}>
-                <Image src={`${img.url}.jpg`} rounded fluid />
-                <Image alt={`${img.user.profile_image}`} src={'https://avatars.dicebear.com/v2/male/:seed.svg'} thumbnail fluid />
-              </div>
+              <Card key={img.id} border="light">
+                <Card.Img variant="top" src={`${img.url}.jpg`} />
+                <Card.Body>
+                  <Card.Text>
+                    by {img.user.name} <br />
+                    Location: {img.user.location || "Not Available"}
+                  </Card.Text>
+                  <Image className="avatar" src={`${img.user.profile_image}.webp`} rounded />
+                </Card.Body>
+              </Card>
           ))
         }
-            </Col>
+
           </Row>
 
     </Container>

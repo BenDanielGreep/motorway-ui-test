cddb68dbecf354e43ddaab87a13aaaf14c12f7a8
Ben Daniel-Greep <bendanielgreep@gmail.com>
2021-09-23 10:45:46 +0100

    image and backup avatar appear inside react-bootstrap component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
+
 
 const App = () => {
   const [images, setImages] = useState();
@@ -17,17 +22,28 @@ const App = () => {
   }, []);
 
   return (
-    <div className='app'>
+    <Container className='app' >
+          <Row>
+            <Col  xs={6} md={4}>
       {
         images && images.map(img => (
-          <div key={img.id} >
-            <img src={`${img.url}.jpg`} alt=''/>
-            <img src={`${img.user.profile_image}.webp`} alt=''/>
-          </div>
-        ))
-      }
-    </div>
+          // <div key={img.id} >
+          //   <img src={`${img.url}.jpg`} alt=''/>
+          //   <img src={`${img.user.profile_image}.webp`} alt=''/>
+          // </div>
+              <div key={img.id}>
+                <Image src={`${img.url}.jpg`} rounded fluid />
+                <Image alt={`${img.user.profile_image}`} src={'https://avatars.dicebear.com/v2/male/:seed.svg'} thumbnail fluid />
+              </div>
+          ))
+        }
+            </Col>
+          </Row>
+
+    </Container>
+    
   );
 }
+
 
 export default App;
